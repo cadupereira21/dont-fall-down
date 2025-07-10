10d8857d17f28dfd0c7bf8028b5a58a6211d1e8d
Carlos Eduardo A Pereira <carloseduardo2101@gmail.com>
2025-07-10 08:41:17 -0300

    Fine tunning movement prototype 1 for mobile device

--- Assets/Scripts/Game Scene/CameraController.cs
+++ Assets/Scripts/Game Scene/CameraController.cs
@@ -1,21 +1,26 @@
+using System;
 using UnityEngine;
 
 namespace Game_Scene {
     public class CameraController : MonoBehaviour {
         
         [SerializeField]
-        [Range(10, 200)]
-        private float rotationSpeed = 100;
+        [Range(1, 100)]
+        private float rotationSpeed = 50;
 
         private Touch _lastTouch;
+
+        private void Awake() {
+            #if UNITY_EDITOR
+                rotationSpeed = rotationSpeed * 5;
+            #endif
+        }
 
         // Update is called once per frame
         private void Update() {
             if (Input.touchCount <= 0) return;
             
             Touch touch = Input.GetTouch(0);
-            
-            Debug.Log($"Touch phase: {touch.phase}");
             
             if (touch.phase == TouchPhase.Moved) {
                 Vector2 deltaPosition = touch.position - _lastTouch.position;
