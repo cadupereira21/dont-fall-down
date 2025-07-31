4f61f8c56bf2f3fe49a4e04c84b235cdec0aefa0
Carlos Eduardo A Pereira <carloseduardo2101@gmail.com>
2025-07-31 20:39:54 -0300

    Implement logger

---
+++ Assets/Scripts/Logger/ILogger.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Logger {
+    public interface ILogger {
+        void Info(string message);
+        void Warning(string message);
+        void Error(string message);
+        
+        void Log(string message, [CallerMemberName] string member = "", [CallerFilePath] string file = "", [CallerLineNumber] int line = 0) {
+            UnityEngine.Debug.Log($"[{System.IO.Path.GetFileName(file)}:{line} - {member}] {message}");
+        }
+    }
+}
---
+++ Assets/Scripts/Logger/UnityLogger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Logger {
+    public class UnityLogger : ILogger {
+        public void Info(string message) {
+            Debug.Log($"{message}");
+        }
+        public void Warning(string message) {
+            Debug.LogWarning($"{message}");
+        }
+        public void Error(string message) {
+            Debug.LogError($"{message}");
+        }
+    }
+}
