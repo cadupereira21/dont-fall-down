99e5a680a7a5d6ace3bfef69edbe667e700295d4
Carlos Eduardo A Pereira <carloseduardo2101@gmail.com>
2025-08-01 09:56:17 -0300

    Implement initial menu with level selector

---
+++ Assets/Scripts/Game Scene/UI/LevelSelector/LevelButton.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Game_Scene.UI.LevelSelector {
+    [RequireComponent(typeof(Button), typeof(Outline))]
+    public class LevelButton : MonoBehaviour {
+        private Button _button;
+        private Outline _outline;
+        
+        private readonly Color _selectedColor = new Color(255, 0, 0, 255);
+        private readonly Color _unselectedColor = new Color(255, 0, 0, 0);
+        
+        private int _levelIndex;
+        
+        public static readonly UnityEvent<int> OnLevelSelected = new ();
+        
+        public void Init(int index) {
+            _levelIndex = index;
+            _button.onClick.AddListener(() => OnLevelSelected.Invoke(_levelIndex));
+        }
+
+        public void Awake() {
+            _button = this.GetComponent<Button>();
+            _outline = this.GetComponent<Outline>();
+        }
+
+        private void OnEnable() {
+            _outline.effectColor = _unselectedColor;
+        }
+
+        public void SelectLevel() {
+            _outline.effectColor = _selectedColor;
+        }
+
+        public void UnselectLevel() {
+            _outline.effectColor = _unselectedColor;
+        }
+    }
+}
---
+++ Assets/Scripts/Game Scene/UI/LevelSelector/LevelSelector.cs
@@ -0,0 +1,47 @@
+﻿using Logger;
+using UnityEngine;
+using ILogger = Logger.ILogger;
+
+namespace Game_Scene.UI.LevelSelector {
+    public class LevelSelector : MonoBehaviour {
+        
+        private LevelButton[] _levelButtons;
+        
+        private LevelButton _selectedLevel;
+        
+        public int SelectedLevelNumber { get; private set; } = -1;
+        
+        public void Awake() {
+            _levelButtons = this.GetComponentsInChildren<LevelButton>();
+        }
+
+        private void Start() {
+            for (int i = 0; i < _levelButtons.Length; i++) {
+                int index = i; // Capture the current index
+                _levelButtons[i].Init(index);
+            }
+        }
+
+        private void OnEnable() {
+            LevelButton.OnLevelSelected.AddListener(SelectLevel);
+        }
+        
+        private void OnDisable() {
+            LevelButton.OnLevelSelected.RemoveListener(SelectLevel);
+        }
+
+        private void SelectLevel(int index) {
+            if (index == SelectedLevelNumber - 1) return;
+
+            if (_selectedLevel != null) {
+                _selectedLevel.UnselectLevel();
+            }
+
+            _selectedLevel = _levelButtons[index];
+            _selectedLevel.SelectLevel();
+            
+            SelectedLevelNumber = index+1;
+        }
+        
+    }
+}
